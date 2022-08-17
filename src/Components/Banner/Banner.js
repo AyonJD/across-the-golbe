f544bd6a21db130a43ad95120b57300fb8542e7f
Ayon Jodder <ayonjodder177@gmail.com>
2022-08-17 14:48:49 +0600

    Banner done

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div className='banner_section position-relative'>
+            <div className="position-absolute banner_text">
+                <h1 className='text-white'>Computer Engineering</h1>
+                <p className='text-white fw-normal'>142,765 Computer Engineers follow this</p>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
