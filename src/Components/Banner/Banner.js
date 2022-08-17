3b0bcb53de790d7ff9295976c4fd862dc0091783
Ayon Jodder <ayonjodder177@gmail.com>
2022-08-17 15:13:37 +0600

    Join group section for mobile done

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { FaArrowLeft } from 'react-icons/fa';
 
 const Banner = () => {
     return (
         <div className='banner_section position-relative'>
+            <div className="join_group_section position-absolute d-flex align-items-center pt-5">
+                <FaArrowLeft className='text-white fs-4' />
+                <button className="join_group_button btn btn-success">Join Group</button>
+            </div>
             <div className="position-absolute banner_text">
                 <h1 className='text-white'>Computer Engineering</h1>
                 <p className='text-white fw-normal'>142,765 Computer Engineers follow this</p>
